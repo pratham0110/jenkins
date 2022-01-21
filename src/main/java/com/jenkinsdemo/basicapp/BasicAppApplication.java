9d822e12538b1bf340958871ffb9eb4235e3e3d4
Pulkit <pulkit.pushkarna@tothenew.com>
2022-01-21 17:00:26 +0530

    v3 commit

--- src/main/java/com/jenkinsdemo/basicapp/BasicAppApplication.java
+++ src/main/java/com/jenkinsdemo/basicapp/BasicAppApplication.java
@@ -11,7 +11,7 @@ public class BasicAppApplication {
 
 	@GetMapping("/")
 	String index(){
-		return "Hello Jenkins V2";
+		return "Hello Jenkins V3";
 	}
 
 	public static void main(String[] args) {
