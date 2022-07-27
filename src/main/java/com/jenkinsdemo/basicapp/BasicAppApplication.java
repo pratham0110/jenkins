5c04d755de2379fce6dc140957986b649ab49eae
pratham0110 <prathamrajhasija@gmail.com>
2022-07-27 13:05:30 +0530

    v9 version

--- src/main/java/com/jenkinsdemo/basicapp/BasicAppApplication.java
+++ src/main/java/com/jenkinsdemo/basicapp/BasicAppApplication.java
@@ -11,7 +11,7 @@ public class BasicAppApplication {
 
 	@GetMapping("/")
 	String index(){
-		return "Hello Jenkins V8";
+		return "Hello Jenkins V9";
 	}
 
 	public static void main(String[] args) {
